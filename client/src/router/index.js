cf5952fa3df24c703a2173ef41b032ef3642d15d
Your Name <you@example.com>
2025-04-10 22:29:57 -0400

    Add new component for adding data

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Add from '@/components/Add.vue'
 
 Vue.use(Router)
 
@@ -14,7 +15,7 @@ export default new Router({
     {
       path: '/add',
       name: 'Add',
-      component: () => import('@/components/Add')
+      component: Add
     }
   ]
 })
