b94eb3b27e14dc0d50253f126665be038a5b0300
Your Name <you@example.com>
2025-04-10 22:34:02 -0400

    this was named wrong

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import Add from '@/components/Add.vue'
+import AddMoviePage from '@/components/AddMoviePage'
 
 Vue.use(Router)
 
@@ -15,7 +15,7 @@ export default new Router({
     {
       path: '/add',
       name: 'Add',
-      component: Add
+      component: AddMoviePage
     }
   ]
 })
